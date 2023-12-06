ff0a92dbe75e2c9eb7c683f254b5554964e62c71
PabloGo90 <140414700+PabloGo90@users.noreply.github.com>
2023-12-06 16:10:31 -0300

    Update BookingContext.cs

--- Models/BookingContext.cs
+++ Models/BookingContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TattooStudioBooking.Helpers;
 
 namespace TattooStudioBooking;
 
@@ -27,6 +28,8 @@ public class BookingContext : DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        Security security = new Security();
+        
         modelBuilder.Entity("TattooStudioBooking.Bloque").HasData(new Bloque { Id = 1, Nombre = "Temprano", HoraDesde = 8, HoraHasta = 10, ActivoFindesem = true });
         modelBuilder.Entity("TattooStudioBooking.Bloque").HasData(new Bloque { Id = 2, Nombre = "Temprano", HoraDesde = 10, HoraHasta = 12, ActivoFindesem = true });
         modelBuilder.Entity("TattooStudioBooking.Bloque").HasData(new Bloque { Id = 3, Nombre = "Tarde", HoraDesde = 12, HoraHasta = 14, ActivoFindesem = true });
@@ -37,13 +40,13 @@ public class BookingContext : DbContext
         modelBuilder.Entity("TattooStudioBooking.Bloque").HasData(new Bloque { Id = 8, Nombre = "Noche", HoraDesde = 22, HoraHasta = 24, ActivoFindesem = true });
 
 
-        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 1, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Danu", PasswordStored = "1234" });
-        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 2, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Joshua", PasswordStored = "1234" });
-        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 3, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Tere", PasswordStored = "1234" });
-        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 4, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Ales", PasswordStored = "1234" });
-        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 5, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Gerardo", PasswordStored = "1234" });
+        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 1, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Danu", PasswordStored =  security.Encrypt("pwd", "1234") });
+        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 2, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Joshua", PasswordStored =  security.Encrypt("pwd", "1234") });
+        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 3, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Tere", PasswordStored =  security.Encrypt("pwd", "1234") });
+        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 4, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Ales", PasswordStored =  security.Encrypt("pwd", "1234") });
+        modelBuilder.Entity("TattooStudioBooking.Artista").HasData(new { Id = 5, Instagram = "", IsAdmin = false, Nombre = "nombre", UserName = "Gerardo", PasswordStored =  security.Encrypt("pwd", "1234") });
 
-        modelBuilder.Entity("TattooStudioBooking.Admin").HasData(new { Id = 1, UserName = "Admin", PasswordStored = "1234", IsAdmin = true, Email = "p.guerraogalde@outlook.cl" });
+        modelBuilder.Entity("TattooStudioBooking.Admin").HasData(new { Id = 1, UserName = "Admin", PasswordStored =  security.Encrypt("pwd", "1234"), IsAdmin = true, Email = "p.guerraogalde@outlook.cl" });
 
         modelBuilder.Entity("TattooStudioBooking.PuestoTrabajo").HasData(new { Id = 1, Nombre = "Puesto Danu" });
         modelBuilder.Entity("TattooStudioBooking.PuestoTrabajo").HasData(new { Id = 2, Nombre = "Puesto Rulos" });
